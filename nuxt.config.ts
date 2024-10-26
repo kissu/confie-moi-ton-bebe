20a281bbb71786e1e8b0fbc528e4c60f9c67abfd
Konstantin BIFERT <hello@kissu.io>
2024-10-26 18:25:55 +0200

    build(tools): smal optimization for Cloudflare

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image']
+  modules: ['@nuxt/eslint', '@nuxt/image'],
+  nitro: {
+    prerender: {
+      autoSubfolderIndex: false
+    }
+  }
 })

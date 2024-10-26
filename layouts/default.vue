974c75c53d8757af02683245a228fb4c82514f63
Konstantin BIFERT <hello@kissu.io>
2024-10-26 18:37:00 +0200

    chore: try out the routes + nuxt image module

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <p>Should be working bro</p>
+    <slot />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <div>hello there mate! :3</div>
+    <NuxtImg src="/avatar.jpg" :modifiers="{ grayscale: true, tint: '#00DC82' }" />
+  </div>
+</template>
